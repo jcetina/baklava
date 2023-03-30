af9f2023e8e8da25c8fda366a71fb6dee61ee03c
Jason Cetina <jcetina@github.com>
2023-03-29 17:49:56 -0700

    +remote

--- providers.tf
+++ providers.tf
@@ -5,6 +5,17 @@ terraform {
       version = "=3.7.0"
     }
   }
+
+  required_version = ">= 1.3.4"
+
+
+  backend "remote" {
+    organization = "jcetina"
+
+    workspaces {
+      name = "gh_jcetina_baklava"
+    }
+  }
 }
 
 # Configure the Microsoft Azure Provider
