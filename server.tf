5669d589b6a273e9e8a4833546fbb16477d4c862
Jason Cetina <jcetina@github.com>
2023-03-31 10:21:15 -0700

    fix image

--- server.tf
+++ server.tf
@@ -45,7 +45,7 @@ resource "azurerm_linux_virtual_machine" "examserverple" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
+    offer     = "0001-com-ubuntu-server-focal"
     sku       = "20.04-LTS"
     version   = "latest"
   }
