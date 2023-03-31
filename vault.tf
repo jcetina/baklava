0a6a1dc697a1920e0f347cd4f5d86f0834a0128f
Jason Cetina <jcetina@github.com>
2023-03-31 16:59:20 -0700

    add perms

--- vault.tf
+++ vault.tf
@@ -23,7 +23,14 @@ resource "azurerm_key_vault_access_policy" "vault_access_policy" {
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azurerm_client_config.current.object_id
 
-
+  key_permissions = [
+    "Create",
+    "Delete",
+    "Get",
+    "Purge",
+    "Recover",
+    "Update",
+  ]
 
   secret_permissions = [
     "Set",
