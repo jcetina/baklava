358a50559ecd077eaa6d581c27c24cc5e942d4a3
Jason Cetina <jcetina@github.com>
2023-03-31 15:26:13 -0700

    +unique_name

--- vault.tf
+++ vault.tf
@@ -1,5 +1,5 @@
 resource "azurerm_key_vault" "vault" {
-  name                       = "bastion-vault"
+  name                       = "bastion-vault-${substr(sha1(random_id.bastion_ssh_key.hex), 0, 8)}}"
   location                   = data.azurerm_resource_group.rg.location
   resource_group_name        = data.azurerm_resource_group.rg.name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
